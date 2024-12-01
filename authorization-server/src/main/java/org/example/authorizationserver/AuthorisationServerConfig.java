a904c83b0213917fe4097359778fbaadce7c0013
irinapodoprigora <ukrira@gmail.com>
2024-12-01 21:19:33 +0000

    added client module

--- authorization-server/src/main/java/org/example/authorizationserver/AuthorisationServerConfig.java
+++ authorization-server/src/main/java/org/example/authorizationserver/AuthorisationServerConfig.java
@@ -25,8 +25,8 @@ public class AuthorisationServerConfig {
     @Bean
     public RegisteredClientRepository registeredClientRepository() {
         RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId("task-client")
-                .clientSecret(passwordEncoder().encode("task-secret"))
+                .clientId("eh-college-client")
+                .clientSecret(passwordEncoder().encode("eh-college-secret"))
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
                 .scope("read")
@@ -63,20 +63,4 @@ public class AuthorisationServerConfig {
 
         return http.build();
     }
-
-    @Bean
-    @Order(2)
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
-            throws Exception {
-        http
-                .authorizeHttpRequests((authorize) -> authorize
-                        .anyRequest().authenticated()
-                )
-                // Form login handles the redirect to the login page from the
-                // authorization server filter chain
-                .formLogin(Customizer.withDefaults());
-
-        return http.build();
-    }
-
 }
